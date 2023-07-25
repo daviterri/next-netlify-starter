fc5238d485f8e8eaa4c44078001d5e4ce80ac592
daviterri <48158957+daviterri@users.noreply.github.com>
2023-07-25 10:52:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          index.html
+          <code>index.html</code>
         </p>
       </main>
 
