5fc5fb097c90d37ece8ea4180411a65d9e28767a
daviterri <48158957+daviterri@users.noreply.github.com>
2023-07-26 08:22:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,34 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div>
+      <Head>
+        <title>Home Page</title>
+        {/* Add meta tags, CSS, or other head elements */}
+      </Head>
+      <header>
+        {/* Create a navigation bar or any header content */}
+        <nav>
+          <ul>
+            <li>
+              {/* Add a link to the rejuvenating page */}
+              <Link href="/rejuvenating">
+                <a>Rejuvenating</a>
+              </Link>
+            </li>
+            {/* Add more navigation links if needed */}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        {/* Add your main content */}
+      </main>
+      <footer>
+        {/* Add a footer section with relevant information */}
+      </footer>
+    </div>
+  );
+};
 
       <Footer />
     </div>
